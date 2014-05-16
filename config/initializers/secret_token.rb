79ebce146d523a25248a8e54083d582b934f2b16
chandrkant123 <chandrkant321@gmail.com>
2014-05-16 17:18:01 +0530

    datatable use

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maltidbuse::Application.config.secret_token = '65cef2528361afd010456104a7a48b492531150b77decff21903135f7096c02208cbe8646dc4d2cf2d85430d52cf7cf7687f8cce99818c4a73c62c9b518a044a'
